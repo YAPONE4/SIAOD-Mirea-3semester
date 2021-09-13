5ef5906a4acb675fce9faec696eaf5d0c3d35d13
YAPONE4 <lxprin@gmail.com>
2021-09-13 14:03:54 +0300

    Task1 Done

--- 1task.cpp
+++ 1task.cpp
@@ -28,8 +28,9 @@ void coutp(unsigned int x)
 
 int main()
 {
+    cout << "Prakticheskaya rabota no1. Prin Alexey IKBO-11-20. Var 4" << endl << endl;
     // Ex.1
-    cout << "Ex.1: ";
+    cout << "Ex.1: " << endl << " n = 0x1CBD = 0001 1100 1011 0101" << endl << "Result: ";
     unsigned short n = 0x1CB5; // 0001 1100 1011 0101
     unsigned short mask = 0x5555; // 101 0101 0101 0101
     /*unsigned mask = 1;
@@ -41,33 +42,35 @@ int main()
     coutp(n);
     cout << endl << endl;
     // Ex.2
-    cout << "Ex.2: ";
+    cout << "Ex.2: " << endl << "Type your n1: ";
     unsigned short n1;
     cin >> n1;
     // unsigned short mask2 = 0x7E1F; // 111 1110 0001 1111
     unsigned short mask2 = ~((~(~0 << 4)) << (5));
     n1 = n1 & mask2;
+    cout << "Result: ";
     coutp(n1);
     cout << endl << endl;
     // Ex.3
-    cout << "Ex.3: ";
+    cout << "Ex.3: " << endl << "Type your n2: ";
     unsigned short n2;
     cin >> n2;
     n2 = n2 << 5;
+    cout << "Result: ";
     coutp(n2);
     // Ex.4
-    cout << endl << endl << "Ex.4: ";
+    cout << endl << endl << "Ex.4: " << endl << "Type your n3: ";
     unsigned short n3;
     cin >> n3;
     n3 = n3 >> 5;
-    cout << n3 << endl;
+    cout << "Result: ";
     coutp(n3);
-    // Ex.5
+    // Ex.5s
     cout << endl << endl << "Ex.5:" << endl << "n4 = ";
     unsigned int n4, nc, x, mask4, count;
     count = 0;
     cin >> n4;
-    cout << "n = ";
+    cout << "nc = ";
     cin >> nc;
     x = n4;
     while (x != 0)
@@ -77,5 +80,6 @@ int main()
     }
     mask4 = (1 << count) >> (count - nc + 1);
     n4 = n4 | mask4;
+    cout << "Result: ";
     coutp(n4);
 }
