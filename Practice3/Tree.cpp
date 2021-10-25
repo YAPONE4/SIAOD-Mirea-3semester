7ae4ce70335a55499ac0c68ddf42356ea24df25a
YAPONE4 <lxprin@gmail.com>
2021-10-25 18:11:32 +0300

    a

--- Practice3/Tree.cpp
+++ Practice3/Tree.cpp
@@ -4,7 +4,23 @@ using namespace std;
 
 class Tree {
     struct Node {
-        int num;
-        
+        int n;
+        Node* child = nullptr;
+        Node* child2 = nullptr;
+        Node* parent = nullptr;
     };
+
+    int *m;
+    int num;
+    Node *root;
+
+    Tree(int n, int *m, int num) {
+        this->m = m;
+        this->root->n = n;
+        this->num = num;
+    }
+
+    int createTree(Node *node, int sch = 1) {
+        
+    }
 };
